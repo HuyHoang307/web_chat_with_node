b8f51852be4ef62dd16750dc70ce7fbd824ad76f
HuyHoang307 <doantienhuyhoang@gmail.com>
2019-11-16 15:00:53 +0700

    [1.0.0] Add static page

--- src/server.js
+++ src/server.js
@@ -1,23 +1,20 @@
 import express from "express";
 import ConnectDB from "./config/connectDB";
-import ContactModel from "./models/contact.model"
+import configViewEngine from "./config/viewEngine";
 
+//Init app
 let app = express();
 //connect to mongo
 ConnectDB();
 
-app.get("/test-database", async (req, res) => {
-  try {
-    let item = {
-      userId: "123415531",
-      contactId: "123452415514546",
-    };
-    console.log(ContactModel.createNew(item));
-    let contact = await ContactModel.createNew(item);
-    res.send(contact);
-  } catch (error) {
-    console.log(error);
-  }
+//config view engine
+configViewEngine(app);
+
+app.get("/", (req, res) => {
+  return res.render("main/master");
+});
+app.get("/login-register", (req, res) => {
+  return res.render("auth/loginRegister");
 });
 
 app.listen(process.env.APP_PORT, process.env.APP_HOST, ()=>{
